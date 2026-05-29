8bde073d879ef6d3564412d73d31ac991cbee43d
Athlas <dawrutko@gmail.com>
2026-05-29 21:43:26 +0200

    feat: parallax stars background fix: button layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Home from "@/pages/Home/Home.jsx";
+import StarBackground from "@/components/StarBackground/StarBackground.jsx";
 
 function App()
 {
     return (
         <Router>
+            <StarBackground />
+
             <Routes>
                 <Route path="/home" element={<Home />}/>
                 <Route path="/" element={<Home />}/>
---
+++ src/components/StarBackground/StarBackground.jsx
@@ -0,0 +1,75 @@
+import "@/styles/globals.scss";
+import "./StarBackground.scss";
+import {useEffect, useRef} from "react";
+
+const STARS_PER_LAYER = 200;
+const SPEED = 0.2;
+
+function StarBackground()
+{
+    const canvasRefs = useRef([]);
+
+    useEffect(() =>
+    {
+        const canvases = canvasRefs.current.map(c => c);
+
+        const dpr = window.devicePixelRatio || 1; // handle device pixel ratio
+
+        canvases.forEach(canvas =>
+        {
+            // canvas size
+            const rect = canvas.getBoundingClientRect();
+            canvas.width = rect.width * dpr;
+            canvas.height = rect.height * dpr;
+
+            const ctx = canvas.getContext("2d");
+            ctx.setTransform(dpr, 0, 0, dpr, 0, 0); // scaling canvas down (compensating for size * dpr)
+
+            // draw stars randomly
+            for (let i = 1; i <= STARS_PER_LAYER; i++)
+            {
+                const x = Math.random() * rect.width;
+                const y = Math.random() * rect.height;
+                const color = Math.floor(Math.random() * 245 + 10); // random brightness
+
+                ctx.fillStyle = `rgb(${color}, ${color}, ${color})`;
+                ctx.fillRect(x, y, 1, 1);
+            }
+        });
+
+        let scheduledFrame = null;
+
+        const handleScroll = () =>
+        {
+            if (scheduledFrame) return;
+
+            scheduledFrame = requestAnimationFrame(() =>
+            {
+                scheduledFrame = null;
+
+                const scrollY = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+
+                canvases.forEach((canvas, index) =>
+                {
+                    canvas.style.transform = `translateY(${scrollY * SPEED * (index + 2)}px)`; // introduce parallax
+                });
+            });
+        }
+
+        window.addEventListener("scroll", handleScroll)
+        return () => window.removeEventListener("scroll", handleScroll);
+
+    }, []);
+
+    return <div className="star-background">
+        {
+            [0, 1, 2].map((i) =>
+                <canvas
+                    key={i}
+                    ref={c => canvasRefs.current[i] = c}
+                />)
+        }
+    </div>;
+}
+
+export default StarBackground
