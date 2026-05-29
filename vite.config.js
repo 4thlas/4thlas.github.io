aa31db5f2bdd01ac863c53d18f229750c384e623
Athlas <dawrutko@gmail.com>
2026-05-29 19:33:59 +0200

    refactor: simplify ScrollDownPrompt.scss

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
@@ -8,5 +8,7 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src')
         }
     },
-  plugins: [react()],
+    plugins: [
+        react(),
+    ],
 })
