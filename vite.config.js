470955531689499af73e9bbf7a7770c013236beb
Athlas <dawrutko@gmail.com>
2026-06-14 20:30:18 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from "path";
 
 export default defineConfig({
+    base: '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src')
@@ -10,6 +11,5 @@ export default defineConfig({
     },
     plugins: [
         react(),
-    ],
-    base: '/'
+    ]
 })
